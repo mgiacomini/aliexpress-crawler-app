4857be08e6d7fed04c1f98e02266e1d8d4b9a1b5
Jessica Moura De Sousa <jessicamoura15@gmail.com>
2016-06-23 03:13:55 -0300

    Creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,7 @@ wordpress = Wordpress.create(name: "Biju Chique",
                  url: "http://bijuchique.com",
                  consumer_key: "ck_7c41a19e24c7f10ad43f9a212ca03409",
                  consumer_secret: "cs_0bf0e3bdee1bf8e20c0518e4065cd654")
-aliexpress = Aliexpresss.create(name: "Jessica",
+aliexpress = Aliexpress.create(name: "Jessica",
                                 email: "jessicams91@hotmail.com",
                                 password: "claudiams")
-
 Crawler.create(aliexpress: aliexpress, wordpress: wordpress)
