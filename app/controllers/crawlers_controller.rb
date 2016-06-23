118b900e43d543328e257c8821a69bb868f5f6e9
Jessica Moura De Sousa <jessicamoura15@gmail.com>
2016-06-23 19:26:07 -0300

    linked task to controller

--- app/controllers/crawlers_controller.rb
+++ app/controllers/crawlers_controller.rb
@@ -31,7 +31,7 @@ class CrawlersController < ApplicationController
   end
 
   def run
-    Rake::Task['crawler.run'].invoke
+    Rake::Task['crawler:run'].invoke
     redirect_to crawlers_path, notice: "#{orders.count} pedidos processados"
   end
 
