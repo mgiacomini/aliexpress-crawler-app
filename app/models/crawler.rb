fb2ad7a03b83552f7dabaf63bbbd863ef9754774
Jessica Moura De Sousa <jessicamoura15@gmail.com>
2016-08-31 22:33:03 -0300

    fix product stock

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -44,8 +44,9 @@ class Crawler < ActiveRecord::Base
               self.set_options user_options
               #Ações dos produtos
               p "Adicionando #{quantity} ao carrinho"
+              raise "Erro de estoque, produto #{item["name"]} não disponível na aliexpress!" if !@b.text_field(name: 'quantity').present? #Verifica disponibilidade
               self.add_quantity quantity
-              raise "Erro de estoque, produto #{item["name"]} não disponível na aliexpress!" if !@b.text_field(name: 'quantity').present? || (@b.text_field(name: 'quantity').value.to_i != quantity)  #Verifica quantidade
+              raise "Erro de estoque, produto #{item["name"]} não disponível na aliexpress!" if @b.text_field(name: 'quantity').value.to_i != quantity  #Verifica quantidade
               p 'Adicionando ao carrinho'
               self.add_to_cart
             rescue => e
