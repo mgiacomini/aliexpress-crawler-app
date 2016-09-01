d87010a95f3e46fdbc2afa32bec5bc43f2e72dfa
Jessica Moura De Sousa <jessicamoura15@gmail.com>
2016-08-31 21:59:26 -0300

    fix and shipping option implemented

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,18 +1,31 @@
 class ProductType < ActiveRecord::Base
   belongs_to :product
 
-  def mobile_link
-    link = self.aliexpress_link.gsub("?","/").gsub("_","/").split("/")
-    id = link.select{|s| s.include?(".html")}.first
-    item = link.select{|s| s.include?("-")}.first
-    "https://m.aliexpress.com/item/#{link}"
+  def parsed_link
+    "http://pt.aliexpress.com/item/#{link_item}/#{link_id}"
   end
 
-  def parsed_link
+  def link_id
+    process_link[:id]
+  end
+
+  def link_item
+    process_link[:item]
+  end
+
+  def process_link
     link = self.aliexpress_link.gsub("?","/").gsub("_","/").split("/")
     id = link.select{|s| s.include?(".html")}.first
     item = link.select{|s| s.include?("-")}.first
-    "http://pt.aliexpress.com/item/#{item}/#{id}"
+
+    {id: id,item: item}
+  end
+
+
+  def clear_errors(order_items)
+    order_items.each do |item|
+      item[:product_type].update(product_errors: 0)
+    end
   end
 
   def add_error
