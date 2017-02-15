96b087d7cf83d899841a153b8c76cc320bbc5d2c
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-15 12:52:14 -0200

    skip verify_authenticity_token for track route

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   skip_before_action :authenticate_user!, only: :track
+  skip_before_filter :verify_authenticity_token, only: :track
 
   def index
     @orders = Order.tracked.order(:created_at)
