2d5915d615ba01ff9f7c0c60cb63775947e7a773
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-03-09 10:36:43 -0300

    rollback Crawler#find_product_type_by_item where query

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -382,12 +382,7 @@ class Crawler < ActiveRecord::Base
     end
 
     product_types = product.product_types
-
-    if name.instance_of? Array
-      product_type = product_types.where('lower(name) IN ?', name).try(:first)
-    else
-      product_type = product_types.where('lower(name) = ?', name).try(:first)
-    end
+    product_type = product_types.where('lower(name) = ?', name).try(:first) if name.class == String
 
     if product_type
       @log.add_message "Variação ##{product_type.id} selecionada"
