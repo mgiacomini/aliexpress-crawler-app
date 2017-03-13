7496470592b0debe7bb8c694bbf750dccc51f8f4
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-03-13 13:14:04 -0300

    adding Wordpress#get_product method to retrieve woocommerce specific product

--- app/models/wordpress.rb
+++ app/models/wordpress.rb
@@ -13,13 +13,13 @@ class Wordpress < ActiveRecord::Base
 
   def woocommerce
     woocommerce = WooCommerce::API.new(
-      self.url, #Url do site
-      self.consumer_key, #Consumer Key
-      self.consumer_secret, #Consumer Secret
-      {
-        wp_api: true,
-        version: "wc/v1" #Versão da API
-      }
+        self.url, #Url do site
+        self.consumer_key, #Consumer Key
+        self.consumer_secret, #Consumer Secret
+        {
+            wp_api: true,
+            version: "wc/v1" #Versão da API
+        }
     )
     woocommerce
   end
@@ -38,6 +38,10 @@ class Wordpress < ActiveRecord::Base
     all_products
   end
 
+  def get_product(id)
+    woocommerce.get("products/#{id}").parsed_response
+  end
+
   def update_order order, order_nos
     self.complete_order order
     self.update_note order, order_nos
@@ -47,7 +51,7 @@ class Wordpress < ActiveRecord::Base
 
   def update_note order, ali_order_num
     #Atualiza pedidos no wordpress com o numero dos pedidos da aliexpress
-    data = { note: ali_order_num }
+    data = {note: ali_order_num}
 
     #POST em order notes
     woocommerce.post("orders/#{order["id"]}/notes", data).parsed_response
@@ -57,14 +61,14 @@ class Wordpress < ActiveRecord::Base
   # *wordpress_reference* is the wordpress order id
   def update_tracking_number_note wordpress_reference, tracking_number
     #Atualiza o código de rastreio do pedido
-    data = { note: "Código de rastreio: #{tracking_number}" }
+    data = {note: "Código de rastreio: #{tracking_number}"}
     #POST em order notes
     woocommerce.post("orders/#{wordpress_reference}/notes", data).parsed_response
   end
 
   def complete_order order
 
-    data = { status: "completed" }
+    data = {status: "completed"}
     #PUT para mudar a ordem para concluída
     woocommerce.put("orders/#{order["id"]}", data).parsed_response
   end
