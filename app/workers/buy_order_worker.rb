258c31f86bc921dafd87f84a4681989e372aa01c
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-03-23 14:29:46 -0300

    process only enqueued orders on BuyOrderWorker

--- app/workers/buy_order_worker.rb
+++ app/workers/buy_order_worker.rb
@@ -1,9 +1,13 @@
 class BuyOrderWorker
   include Sidekiq::Worker
 
-  def perform(crawler_id, crawler_log_id, order={})
+  def perform(crawler_id, crawler_log_id, order_id)
     crawler = Crawler.find crawler_id
     crawler_log = CrawlerLog.find crawler_log_id
-    crawler.run order, crawler_log
+    order = Order.find order_id
+    if order.enqueued?
+      crawler.run order.metadata, crawler_log
+      order.processed!
+    end
   end
 end
